1cbb7a463ce766ff98b9b877b401c779ae256cbb
aniketsriwastva <aniket.sriwastva@gmail.com>
2023-09-17 20:39:28 +0530

    db setup

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,5 +1,13 @@
+const db = require("../model/db-connection");
 exports.requestToGetData = (req, res) => {
-  res.status(200).json({
-    message: "It works!",
+  // res.status(200).json({
+  //   message: "It works!",
+  // });
+  let sql = "CREATE DATABASE nodemysql";
+  db.query(sql, (err) => {
+    if (err) {
+      throw err;
+    }
+    res.send("Database Created");
   });
 };
