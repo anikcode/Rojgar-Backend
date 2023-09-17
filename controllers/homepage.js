6b30a02a19e2c3fae54f94f1e229ae823eb81f40
aniketsriwastva <aniket.sriwastva@gmail.com>
2023-09-17 20:27:25 +0530

    Revert "db setup"
    This reverts commit d0580de98900a0c568aa32b8ca7af477b17e1477.

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,13 +1,5 @@
-const db = require("../model/db-connection");
 exports.requestToGetData = (req, res) => {
-  // res.status(200).json({
-  //   message: "It works!",
-  // });
-  let sql = "CREATE DATABASE nodemysql";
-  db.query(sql, (err) => {
-    if (err) {
-      throw err;
-    }
-    res.send("Database Created");
+  res.status(200).json({
+    message: "It works!",
   });
 };
