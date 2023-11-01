18ffbf588d9547930ee563fac166753e6c4fc8f7
aniketsriwastva <aniket.sriwastva@gmail.com>
2023-11-02 01:56:05 +0530

    added token validation on api

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,14 @@
 const jwt = require("jsonwebtoken");
 const SECRET_KEY = "aniket@9430";
+const logger = require("../logger");
 
 exports.authenticate = (req, res, next) => {
   try {
     let token = req.headers.authorization;
+    logger.debug(token, "[token hai ye toh]");
     if (token) {
-      token = token.split(" ")[1];
+      //   token = token.split(" ")[1];
+      logger.debug(token, "[token hai ye toh phri se]");
       let user = jwt.verify(token, SECRET_KEY);
     } else {
       res.status(401).json({ message: "Unauthorized User" });
